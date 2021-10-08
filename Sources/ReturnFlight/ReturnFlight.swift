907cb40ef9657b3ec9bdca60b403457b6a2e095b
ytbryan <ytbryan@gmail.com>
2021-10-08 16:06:40 +0800

    public

--- Sources/ReturnFlight/ReturnFlight.swift
+++ Sources/ReturnFlight/ReturnFlight.swift
@@ -2,12 +2,12 @@ struct ReturnFlight {
     var text = "Hello, World!"
 }
 
-func `return`(answer:AnyObject) -> AnyObject{
+public func `return`(answer:AnyObject) -> AnyObject{
     print(answer)
     return answer
 }
 
-func well(something:Any) -> Any{
+public func well(something:Any) -> Any{
     print(something)
     return something
 }
